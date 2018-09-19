ce68c8c14be2bf276e16906a5242727da083a62c
Ksilent467 <piyush1997.com@gmail.com>
2018-09-19 21:50:54 +0530

    Add files via upload

---
+++ js/controller.js
@@ -0,0 +1,44 @@
+app.controller("myCtrl",($scope,myFactory,$location,$route,$rootScope,$timeout)=>{
+  $scope.reload = () =>{
+    var promise = myFactory.get();
+    promise.then(function (data){
+      $scope.result = data;
+      console.log($scope.result);
+    },function (err){
+      $scope.result=  err;
+    });
+    $timeout(function(){
+      $scope.reload();
+    },50000)
+  }
+
+
+
+$scope.showMe=true
+$rootScope.$on('$locationChangeSuccess', function() {
+       $rootScope.actualLocation = $location.path();
+   });
+
+  $rootScope.$watch(function () {return $location.path()}, function (newLocation, oldLocation) {
+       if($rootScope.actualLocation === newLocation) {
+           $scope.change()
+       }
+   });
+
+$scope.check = (x)=>{
+  if($scope.result[x].percent_change_24h >0){
+    console.log("gg")
+  return $scope.above = true; }
+  else {console.log("ggrip")
+    return $scope.below = true
+
+  }
+}
+
+$scope.change=(x)=>{
+  $scope.showMe=!$scope.showMe
+
+  $scope.check(x);
+}
+    $scope.reload();
+});
